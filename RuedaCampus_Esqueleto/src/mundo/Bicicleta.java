8119e079afe4efc5d1e5f00daf24e9718379cab3
Karen Estefany Jacanamejoy Miticanoy <jacanamejoykarenn@gmail.com>
2026-09-14 15:30:55 -0500

    calcularCosto() bicicleta

--- RuedaCampus_Esqueleto/src/mundo/Bicicleta.java
+++ RuedaCampus_Esqueleto/src/mundo/Bicicleta.java
@@ -16,8 +16,35 @@ public class Bicicleta extends Vehiculo {
      * @return TODO: tipo y significado del resultado
      * @throws IllegalArgumentException TODO: condición que la produce
      */
+    
     @Override
     public double calcularCosto(int horas) {
-        throw new UnsupportedOperationException("TODO: implementar el requerimiento.");
+       
+    	double valor=0;
+    	double total_parcial = 0;
+    	double total = 0;
+    	if (horas <= 0 ) {
+    		throw new IllegalArgumentException("El número de horas debe ser mayor a 0");
+    	}
+    	
+    	if (horas <=2) {
+    		valor= 4000; 
+    		total = valor* horas;
+    		
+    	}
+    	
+    	if (horas >2 ) {
+    		valor = 3000;
+   
+    		total_parcial = valor * (horas-2);
+    		total = total_parcial + 8000;
+    		
+    		
+    	}
+    	  
+    	return total;
+    	
+    	
     }
-}
+
+    }
